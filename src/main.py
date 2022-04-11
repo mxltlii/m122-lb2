f697ec978132e6fbbf1dae12ddb017e12de1144a
mxl17 <89915581+mxl17@users.noreply.github.com>
2022-04-11 11:43:50 +0200

    Get Data and write into generated PDF

--- src/main.py
+++ src/main.py
@@ -1,18 +1,30 @@
 import http.client
-import os
+import json
+import pandas as pd
+from fpdf import FPDF
 
 conn = http.client.HTTPSConnection("yh-finance.p.rapidapi.com")
-# Get API Key from system variable
-api_key = os.environ.get('m122_yh_finance_key')
 
 headers = {
     'X-RapidAPI-Host': "yh-finance.p.rapidapi.com",
-    'X-RapidAPI-Key': api_key
+    'X-RapidAPI-Key': "ac50f0a264mshb4d50d8737b1a85p178f48jsne098af8d0093"
     }
 
-conn.request("GET", "/stock/v2/get-chart?interval=5m&symbol=TSLA&range=1mo&region=US", headers=headers)
+conn.request("GET", "/stock/v2/get-summary?symbol=TSLA&region=US", headers=headers)
 
 res = conn.getresponse()
 data = res.read()
 
-print(data.decode("utf-8"))
+jsonData = json.loads(data)
+
+#df = pd.read_json(data.decode("utf-8"))
+#df.info()
+
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font("Arial", size = 20)
+pdf.cell(200, 10, txt = jsonData['price']['shortName'], ln = 1, align = 'C')
+pdf.set_font("Arial", size = 14)
+pdf.cell(200, 10, txt = "Preis: " + jsonData['price']['regularMarketOpen']['fmt'], ln = 2, align = 'L')
+
+pdf.output("test.pdf")
